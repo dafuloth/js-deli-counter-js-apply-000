8732a562f29b53f4cdf30d033ad4ea8a8a25000b
David Thai <david.thai@dafuloth.me>
2018-09-01 05:56:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function nowServing(line) {
 
 function currentLine(line) {
   if (line.length > 0) {
-    // const queue = []
-    // for (let i = 0; i < line.length; i++) {
-    //   queue.push(`${i+1}. ${}`)
-    // }
-    return `The line is currently: 1. ${line.toString().split().join(`, ${1++}. `)}`
+    const customers = []
+    for (let i = 0; i < line.length; i++) {
+      queue.push(`${i+1}. ${line[i]},`)
+    }
+    return `The line is currently:`
   } else {
     return 'The line is currently empty.'
   }
