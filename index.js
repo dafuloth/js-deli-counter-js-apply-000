a24077ad610b9df5ab999af959662c2bd43f1cb4
David Thai <david.thai@dafuloth.me>
2018-09-01 05:31:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,20 @@ function takeANumber(line, name) {
 function nowServing(line) {
   if (line.length > 0) {
     return `Currently serving ${line.shift()}.`
+  } else {
+    return 'There is nobody waiting to be served!'
+  }
+}
+
+
+function currentLine(line) {
+  if (line.length > 0) {
+    let queue = ""
+    for (let i = 0; i < line.length; i++) {
+      queue += `${i+1}. ${line[i],}`
+    }
+    return `The line is currently: ${queue}`
+  } else {
+    return 'The line is currently empty.'
   }
 }
